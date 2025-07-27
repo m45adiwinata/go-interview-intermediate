682f5db7b2ac691c9dcae8ce5736debe740e6aad
Made Mas Adi Winata <m45adiwinata@gmail.com>
2025-07-27 17:01:50 +0700

    prepared generic

--- main.go
+++ main.go
@@ -7,16 +7,14 @@ import (
 
 func main() {
 	n := 30
-	int_list := make([]int, n)
-	for i := range n {
-		x := rand.Intn(6)
-		int_list[i] = x
-	}
+	int_list := generate_list(n)
 	fmt.Println(int_list)
 
-	//kelompokkan tiap bilangan yang sama, contoh:
-	//int_list: [1 3 2 2 5 1 4 4 4 5 3 5 5 4 0 4 4 4 2 4 2 4 4 2 1 5 2 0 3 4]
-	//grouped: [
+	// kelompokkan tiap bilangan yang sama, contoh:
+	// int_list:
+	// [1 3 2 2 5 1 4 4 4 5 3 5 5 4 0 4 4 4 2 4 2 4 4 2 1 5 2 0 3 4]
+	// grouped:
+	// [
 	//         [1 1 1],
 	//         [3 3 3],
 	//         [2 2 2 2 2 2],
@@ -27,6 +25,15 @@ func main() {
 	grouped := [][]int{}
 
 	print_grouped(grouped)
+}
+
+func generate_list(n int) []int {
+	int_list := make([]int, n)
+	for i := range n {
+		x := rand.Intn(6)
+		int_list[i] = x
+	}
+	return int_list
 }
 
 func print_grouped(grouped [][]int) {
